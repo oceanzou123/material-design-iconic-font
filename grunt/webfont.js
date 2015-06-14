8a818489ca32134506fa25a3478c4402f1506857
Sergey Kupletsky <s.kupletsky@gmail.com>
2015-06-15 01:34:44 +0300

    Add webfont builder

---
+++ grunt/webfont.js
@@ -0,0 +1,10 @@
+module.exports = {
+    'Material-Design-Iconic-Font': {
+        src: 'svg/toggle/*.svg',
+        dest: 'dist/font',
+        options: {
+            engine: 'node',
+            fontHeight: 48
+        }
+    }
+};
